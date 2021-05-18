5fd8f1491316fdffb506db37aba2b4812ae84adf
Izaías di Araújo <izaiasousa@gmail.com>
2021-05-18 13:22:38 -0300

    :rocket:

--- src/models/boletim.model.js
+++ src/models/boletim.model.js
@@ -1,17 +1,18 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
 const BoletimSchema = new mongoose.Schema(
   {
     name: {
       type: String,
       required: true,
-      index: true,
-    },
+      index: true
+    }
   },
   {
     timestamps: true,
+    strict: false //values added to our model instance that were not specified
   }
-);
+)
 
-const User = mongoose.model("User", UserSchema);
-export default User;
+const User = mongoose.model('User', UserSchema)
+export default User
