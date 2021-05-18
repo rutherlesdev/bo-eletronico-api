c6f7020b3ee3e152bcb23314345c7de909dce950
Izaías di Araújo <izaiasousa@gmail.com>
2021-05-18 14:53:58 -0300

    :rocket:

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import connect from "./services/db";
+import router from "./routes/boletim.route";
 
 const app = express();
 
@@ -9,5 +10,7 @@ const db = connect();
 app.use(bodyParser());
 app.get("/", (req, res) => {
     res.json("Hello there");
-})
+});
+app.use("/api/v1", router);
+
 app.listen(process.env.PORT || 3000);
